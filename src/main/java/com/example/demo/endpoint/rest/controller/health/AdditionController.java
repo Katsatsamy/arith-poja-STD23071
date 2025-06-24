84420730ea8741c2fa953ec1e6c0ef87a621715b
Katsatsamy <velonyainaemilie@gmail.com>
2025-06-24 09:58:48 +0300

    feat: doing additionController

---
+++ src/main/java/com/example/demo/endpoint/rest/controller/health/AdditionController.java
@@ -0,0 +1,13 @@
+package com.example.demo.endpoint.rest.controller.health;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdditionController {
+    @GetMapping("/add")
+    public long addition(@RequestParam long a, @RequestParam long b) {
+        return a+b;
+    }
+}
